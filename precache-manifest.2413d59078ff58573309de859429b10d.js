a92849c9ec8ec4a88e12aceea385438e631535fe
Anna Orlova <orlovanna412@gmail.com>
2019-09-23 02:43:11 +0100

    Updates

---
+++ precache-manifest.2413d59078ff58573309de859429b10d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c8674c634bd60a0d36ad47ec4001f11",
+    "url": "/planes-coding-challenge/index.html"
+  },
+  {
+    "revision": "02ecb40cad281dd83394",
+    "url": "/planes-coding-challenge/static/css/main.5cd64bcb.chunk.css"
+  },
+  {
+    "revision": "cda6298e274a48089266",
+    "url": "/planes-coding-challenge/static/js/2.160c8d67.chunk.js"
+  },
+  {
+    "revision": "02ecb40cad281dd83394",
+    "url": "/planes-coding-challenge/static/js/main.e7c49397.chunk.js"
+  },
+  {
+    "revision": "22f7bacedebd4fbbaa8e",
+    "url": "/planes-coding-challenge/static/js/runtime-main.fa4049ea.js"
+  }
+]);
